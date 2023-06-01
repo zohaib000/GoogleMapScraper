bcfe2f8d4c0ddc999cf87338d39e260cbc78ae32
Ch vinay <chvinay8288@gmail.com>
2023-06-01 09:26:43 +0530

    fuck you! bastard made me do all the work and escape like an idot bastard It's your whore mom to blame that anyway not you

--- home/new.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-year=datetime.now().year
-month=datetime.now().month
-date=datetime.now().day
-
-
--- map_scrapers/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import History
-
-
-class HistoryUpdateForm(forms.ModelForm):
-    class Meta:
-        model = History
-        fields = "__all__"
